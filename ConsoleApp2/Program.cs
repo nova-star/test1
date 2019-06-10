ade5cfd923651b8bcb7a9c348e74c90bb9f538bc
lynx <lynx@localhost>
2019-06-10 18:21:41 +0700

    Add strings

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -8,11 +8,17 @@ namespace ConsoleApp2
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine(Add(10, 20));
+            Console.WriteLine(AddStrings("one", "two"));
         }
 
         static int Add(int x, int y, int z = 0)
         {
             return x + y + z;
+        }
+
+        static string AddStrings(string x, string y)
+        {
+            return x + y;
         }
     }
 }
