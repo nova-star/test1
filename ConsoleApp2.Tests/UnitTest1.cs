736bb8a351f3f8a190021294a151f53d9a1e4a68
lynx <lynx@localhost>
2019-06-26 18:26:47 +0700

    unit tests

---
+++ ConsoleApp2.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using ConsoleApp2;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace ConsoleApp2.Tests
+{
+    public class ProgramTest
+    {
+        [Fact]
+        public void AddTest()
+        {
+            Program
+
+        }
+    }
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,19 +7,23 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine(Add(10, 20));
-            Console.WriteLine(AddStrings("one", "two"));
-            Console.WriteLine(AddStrings("three", "four"));
+            Console.WriteLine(Lib.Add(10, 20));
+            Console.WriteLine(Lib.AddStrings("one", "two"));
+            Console.WriteLine(Lib.AddStrings("three", "four"));
         }
+    }
 
-        static int Add(int x, int y, int z = 0)
+    public class Lib
+    {
+        public static int Add(int x, int y, int z = 0)
         {
             return x + y + z;
         }
 
-        static string AddStrings(string x, string y)
+        public static string AddStrings(string x, string y)
         {
             return x + y;
         }
+        
     }
 }
